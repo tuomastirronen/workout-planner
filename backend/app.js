7b96d3b0c05f95f9ab54f4be17359f30c0bba1b6
Tuomas Tirronen <tuomas.tirronen@gmail.com>
2018-03-21 14:29:01 +0200

    install sequelize

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,20 @@
-const express = require('express')
-//const session = require('express-session');
-//const mongoose = require('mongoose');
-//const MongoStore = require('connect-mongo')(session);
-const path = require('path')
-//const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser')
-//const passport = require('passport');
-//const promisify = require('es6-promisify');
-//const flash = require('connect-flash');
-//const expressValidator = require('express-validator');
-const routes = require('./routes/index')
-//const helpers = require('./helpers');
-const errorHandlers = require('./handlers/errorHandlers')
-//require('./handlers/passport');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-// create our Express app
-const app = express()
+// Set up the express app
+const app = express();
 
-// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
-app.use(express.static(path.join(__dirname, 'public')))
+// Log requests to the console.
+app.use(logger('dev'));
 
-// Takes the raw requests and turns them into usable properties on req.body
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
-//app.use(expressValidator());
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
 
-// // The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
-//app.use(flash());
-
-// After allllll that above middleware, we finally handle our own routes!
-app.use('/', routes)
-
-// If that above routes didnt work, we 404 them and forward to error handler
-app.use(errorHandlers.notFound)
-
-// One of our error handlers will see if these errors are just validation errors
-//app.use(errorHandlers.flashValidationErrors);
-
-// Otherwise this was a really bad error we didn't expect! Shoot eh
-if (app.get('env') === 'development') {
-  /* Development Error Handler - Prints stack trace */
-  app.use(errorHandlers.developmentErrors)
-}
-
-// production error handler
-//app.use(errorHandlers.productionErrors);
-
-// done! we export it so we can start the site in start.js
-module.exports = app
+module.exports = app;
