ae01801091905df2e8ce1818455fd663edef1edf
joonissi <joonanissinen123@gmail.com>
2018-03-21 16:28:45 +0200

    frontend mockup

---
+++ frontend/src/components/Content.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Grid, Segment } from 'semantic-ui-react'
+
+const Content = () => (
+  <Grid columns={3} divided>
+    <Grid.Row>
+      <Grid.Column>
+        <Segment>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </Segment>
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+        <Segment>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </Segment>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
+
+export default Content
+
