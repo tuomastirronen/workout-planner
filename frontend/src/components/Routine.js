acffd07d84c0221b4795c0301bde1c4c070ff02b
joonissi <joonanissinen123@gmail.com>
2018-04-12 13:11:46 +0300

    excersises in progress

--- frontend/src/components/Routine.js
+++ frontend/src/components/Routine.js
@@ -1,16 +1,56 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Routine = ({ routine }) => {
-  return (
-    <li>
-      <div>
-        day: {routine.weekday} name: {routine.name}
-      </div>
-      <div>
-        start
-      </div>
-    </li>
-  )
+import { showRoutine } from './../reducers/routineReducer'
+
+class Routine extends React.Component {
+  componentDidMount () {
+    console.log('Routine did mount')
+  }
+  
+  // showRoutine = (event) => {
+  //   //event.preventDefault()
+  //   this.props.showRoutine(event.target.note.value)
+  //   event.target.note.value = ''
+  // }
+
+  showRoutineById = () => {
+    console.log('showRoutineById')
+    console.log(this.props.routine.id)
+    this.props.showRoutine(this.props.user.user.id, this.props.routine.id)
+  }
+
+  render () {
+
+    return (
+      <li>
+        <div>
+          day: {this.props.routine.weekday} name: {this.props.routine.name}
+        </div>
+        <div>
+          <button onClick={this.showRoutineById}>Start</button>
+        </div>
+      </li>
+    )
+  }
 }
 
-export default Routine
+// const Routine = ({ routine }) => {
+//   return (
+
+//   )
+// }
+
+const mapStateToProps = (state) => {
+  return {
+    routines: state.routines,
+    user: state.user
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { showRoutine }
+)(Routine)
+
+//export default Routine
